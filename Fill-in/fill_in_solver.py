243fe1f78e0d6ded16496c019e1490cdbed7bf09
Miłosz Kierepka <milosz.kierepka@gmail.com>
2022-05-23 21:33:17 +0200

    Fill-in: minor improvements to solver

--- Fill-in/fill_in_solver.py
+++ Fill-in/fill_in_solver.py
@@ -14,11 +14,11 @@ class _Gap:
         self.length = length
         self.vertical = vertical
 
-    def get_gap(self, grid: list[list]) -> []:
+    def get_gap(self, grid: list[list]) -> [str]:
         return (
             (grid[self.row + i][self.column] for i in range(self.length))
             if self.vertical
-            else grid[self.row][self.column : (self.column + self.length)]
+            else grid[self.row][self.column : self.column + self.length]
         )
 
     def fill_gap(self, grid: list[list[str]], word: str) -> None:
@@ -26,8 +26,7 @@ class _Gap:
             for i in range(self.length):
                 grid[self.row + i][self.column] = word[i]
         else:
-            for i in range(self.length):
-                grid[self.row][self.column + i] = word[i]
+            grid[self.row][self.column : self.column + self.length] = list(word)
 
 
 class AbstractSolver(ABC):
@@ -38,43 +37,45 @@ class AbstractSolver(ABC):
     def solve(self, crossword: list[list[str]], words: [str]) -> list[list[str]]:
         pass
 
+    def get_gaps(self, crossword: list[list[str]]) -> list[_Gap]:
+        gaps = []
+        height = len(crossword)
+        width = len(crossword[0])
+        for i, row in enumerate(crossword):
+            for j, cell in enumerate(row):
+                if self.separator == cell:
+                    continue
+                # vertical
+                if 0 == i or self.separator == crossword[i - 1][j]:
+                    length = 1
+                    while (
+                        i + length < height
+                        and self.separator != crossword[i + length][j]
+                    ):
+                        length += 1
+                    gaps.append(_Gap(i, j, length, True))
+                # horizontal
+                if 0 == j or self.separator == row[j - 1]:
+                    length = 1
+                    while j + length < width and self.separator != row[j + length]:
+                        length += 1
+                    gaps.append(_Gap(i, j, length, False))
+        return list(filter(lambda gap: 1 < gap.length, gaps))
+
 
 class BacktrackingSolver(AbstractSolver):
-    def solve(self, crossword: list[list[str]], words: [str]) -> list[list[str]]:
-        grid = deepcopy(crossword)
-
-        gaps = []
-        height = len(grid)
-        width = len(grid[0])
-        for i, row in enumerate(grid):
-            for j, cell in enumerate(row):
-                if self.separator != cell:
-                    # vertical
-                    if 0 == i or self.separator == grid[i - 1][j]:
-                        length = 1
-                        while (
-                            i + length < height
-                            and self.separator != grid[i + length][j]
-                        ):
-                            length += 1
-                        gaps.append(_Gap(i, j, length, True))
-                    # horizontal
-                    if 0 == j or self.separator == row[j - 1]:
-                        length = 1
-                        while j + length < width and self.separator != row[j + length]:
-                            length += 1
-                        gaps.append(_Gap(i, j, length, False))
-
-        gaps = list(filter(lambda gap: 1 < gap.length, gaps))
-
+    @staticmethod
+    def group_words(words):
         words_dict = {}
         for word in words:
             words_dict[len(word)] = words_dict.get(len(word), []) + [
                 {"word": word, "used": False}
             ]
+        return words_dict
 
-        if self._check(grid, words_dict, gaps):
-            print("\n".join(["".join(row) for row in grid]))
+    def solve(self, crossword: list[list[str]], words: [str]) -> list[list[str]]:
+        grid = deepcopy(crossword)
+        if self._check(grid, self.group_words(words), self.get_gaps(grid)):
             return grid
 
     def _check(
@@ -82,19 +83,19 @@ class BacktrackingSolver(AbstractSolver):
         crossword: list[list[str]],
         words_dict: dict[int, list[dict]],
         gaps: [_Gap],
-    ):
+    ) -> bool:
         if not gaps:
             return True
         gap = gaps[0]
         missing = "".join(gap.get_gap(crossword))
-        for pos in filter(
+        for option in filter(
             lambda word: (not word["used"]) and re.match(missing, word["word"]),
             words_dict[gap.length],
         ):
-            pos["used"] = True
-            gap.fill_gap(crossword, pos["word"])
+            option["used"] = True
+            gap.fill_gap(crossword, option["word"])
             if self._check(crossword, words_dict, gaps[1:]):
                 return True
             gap.fill_gap(crossword, missing)
-            pos["used"] = False
+            option["used"] = False
         return False
